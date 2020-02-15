2d57cc212831ec63e0b8dc4a09385c3285dd2da7
Aditya-Jha2002 <52704430+Aditya-Jha2002@users.noreply.github.com>
2020-02-15 15:50:42 +0530

    Update CustomColors.java

--- src/view/CustomColors.java
+++ src/view/CustomColors.java
@@ -2,11 +2,7 @@ package view;
 
 import java.awt.*;
 
-/**
- * <p>This class allows others to access its colors</p>
- * @author Created by Tobias on 07/12/15.
- *
- */
+
 public class CustomColors {
     public static Color green = new Color(0, 189, 154);
     public static Color grey = new Color(222, 222, 222);
