3b359b72892606abad2d8a29456869eced7e2cb2
Aditya-Jha2002 <52704430+Aditya-Jha2002@users.noreply.github.com>
2020-02-15 15:49:55 +0530

    Update SQLiteConnector.java

--- src/model/SQLiteConnector.java
+++ src/model/SQLiteConnector.java
@@ -4,13 +4,7 @@ import javax.swing.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- *  <p>This class establishes the connection with the SQLite database</p>
- *
- *  @credit "Programmieren Lernen mit Java - Chapter 14"
- *  @author Tobias Büschel
- *  @verison 0.7
- */
+
 public class SQLiteConnector {
 
     static Connection connection = null;
@@ -18,10 +12,9 @@ public class SQLiteConnector {
     public static Connection dbConnector() {
 
         try {
-            /** load the sqlite-JDBC driver with the help of the class loader */
+         
             Class.forName("org.sqlite.JDBC");
 
-            /** create the database connection */
             connection = DriverManager.getConnection("jdbc:sqlite:PatientDatabase");
             return connection;
 
